de641a589ddb2ebddd9d70cfd81844c313b6d6e2
Adeline Claire MOREL <adelinemorel72@gmail.com>
2023-03-03 21:23:54 +0100

    writing my first testcoverage command line

--- js/samples/unit/unit1.js
+++ js/samples/unit/unit1.js
@@ -4,8 +4,11 @@
  */
 export const sayHello = name => {
     if (!name) {
-        return "Hello, World"
+    return "Hello, World"
     }
-
+    // J'ajoute ici cette ligne dans ma fonction
+    if (name === "Alexandra") {
+    return "Bonjour Alexandra"
+    }
     return `Hello, ${name}`
 }
